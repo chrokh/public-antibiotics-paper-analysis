bb703c5da6fe5f7a3f2b76b60ae1b2393ae14f50
chrokh <christopher.okhravi@gmail.com>
2019-02-05 13:48:45 +0100

    Remove unused variables and simplify (by inlining and not saving revenue and cost values as variables) PV, rPV, NPV, rNPV, and cumulative calculations.

--- src/plot/interventions.R
+++ src/plot/interventions.R
@@ -21,57 +21,38 @@ phase_years$phase <- factor(phase_years$phase, levels=phase_levels, ordered=TRUE
 intervention_levels <- c('NONE', 'P1ER', 'P2ER', 'P3ER', 'P4ER', 'PDMER')
 phase_years$intervention <- factor(phase_years$intervention, levels=intervention_levels)
 
+# Compute: rNPV and other aggregates
 phase_years <- phase_years %>%
   group_by(intervention, subject) %>%
   arrange(subject, t) %>%
-  transmute(time.to         = t - min(t),
-            year            = floor(time.to),
-            # prob
-            cum.prob        = cumprod(prob),
-            prob.to         = cum.prob / prob,
-            # cost
-            cost.cum        = cumsum(cost),
-            cost.rv         = cost * prob.to, # TODO:ok?
-            cost.rv.cum     = cumsum(cost.rv),
-            cost.pv         = cost / ((1 + discount.rate) ^ time.to),
-            cost.rpv        = (cost / ((1 + discount.rate) ^ time.to)) * prob.to,
-            cost.npv        = cumsum(cost.pv),
-            cost.rnpv       = cumsum(cost.rpv), # TODO:ok?
-            # revenue
-            revenue.cum     = cumsum(revenue),
-            revenue.rv      = revenue * prob.to, # TODO:ok?
-            revenue.rv.cum  = cumsum(revenue.rv),
-            revenue.pv      = revenue / ((1 + discount.rate) ^ time.to),
-            revenue.rpv     = (revenue / ((1 + discount.rate) ^ time.to)) * prob.to,
-            revenue.npv     = cumsum(revenue.pv),
-            revenue.rnpv    = cumsum(revenue.rpv), # TODO:ok?
-            # cashflow
-            cashflow.cum    = revenue.cum - cost.cum,
-            cashflow.rv     = revenue.rv - cost.rv,
-            cashflow.rv.cum = revenue.rv.cum - cost.rv.cum,
-            cashflow.pv     = revenue.pv - cost.pv,
-            cashflow.rpv    = revenue.rpv - cost.rpv,
-            cashflow.npv    = revenue.npv - cost.npv,
-            cashflow.rnpv   = revenue.rnpv - cost.rnpv,
+  transmute(time.to = t - min(t),
+            prob.to = cumprod(prob) / prob,
+            cum     = cumsum(revenue) - cumsum(cost),
+            rv      = (revenue * prob.to) - (cost * prob.to),
+            rv.cum  = cumsum(rv),
+            pv      = (revenue - cost) / ((1 + discount.rate) ^ time.to),
+            rpv     = ((revenue - cost) / ((1 + discount.rate) ^ time.to)) * prob.to,
+            npv     = cumsum(pv),
+            rnpv    = cumsum(rpv),
             )
 
 # Save final rnpv
 finals <- phase_years %>%
   group_by(subject, intervention) %>%
-  filter(year == max(year)) %>%
-  summarise(cashflow.cum    = tail(cashflow.cum, n=1),
-            cashflow.rv.cum = tail(cashflow.rv.cum, n=1),
-            cashflow.npv    = tail(cashflow.npv, n=1),
-            cashflow.rnpv   = tail(cashflow.rnpv, n=1),
+  filter(time.to == max(time.to)) %>%
+  summarise(cum    = tail(cum, n=1),
+            rv.cum = tail(rv.cum, n=1),
+            npv    = tail(npv, n=1),
+            rnpv   = tail(rnpv, n=1),
             )
 
 # Compute: intervention size/cost
 control <- finals %>% filter(intervention == 'NONE')
-finals$control.cashflow.cum <- rep(control$cashflow.cum, each=length(intervention_levels))
-finals$intervention_cost    <- finals$cashflow.cum - finals$control.cashflow.cum
+finals$control.cum <- rep(control$cum, each=length(intervention_levels))
+finals$intervention_cost    <- finals$cum - finals$control.cum
 
 # Plot: RNPV ~ intervention cost (log)
-ggplot(finals, aes(intervention_cost, cashflow.rnpv, color=intervention)) +
+ggplot(finals, aes(intervention_cost, rnpv, color=intervention)) +
   geom_point() +
   geom_smooth(method='lm', se=FALSE) +
   scale_y_continuous(trans = 'log',
@@ -82,14 +63,14 @@ ggplot(finals, aes(intervention_cost, cashflow.rnpv, color=intervention)) +
                      minor_breaks = c(1:9 %o% 10^(0:4)))
 
 # Plot: RNPV ~ intervention cost (not log)
-ggplot(finals, aes(intervention_cost, cashflow.rnpv, color=intervention)) +
+ggplot(finals, aes(intervention_cost, rnpv, color=intervention)) +
   geom_point() +
   geom_smooth(method='lm', se=FALSE) +
   scale_y_continuous(breaks=pretty_breaks(n=10)) +
   scale_x_continuous(breaks=pretty_breaks(n=10))
 
 # Plot: RNPV ~ intervention cost (not log, facets)
-ggplot(finals, aes(intervention_cost, cashflow.rnpv, color=intervention)) +
+ggplot(finals, aes(intervention_cost, rnpv, color=intervention)) +
   geom_point() +
   geom_smooth(method='lm', color='black', se=FALSE) +
   facet_wrap(. ~ intervention, scale='free') +
@@ -99,7 +80,7 @@ ggplot(finals, aes(intervention_cost, cashflow.rnpv, color=intervention)) +
         legend.position = 'none')
 
 # Compute: gos/nogos
-finals$go <- factor(finals$cashflow.rnpv >= 0, levels=c(TRUE, FALSE))
+finals$go <- factor(finals$rnpv >= 0, levels=c(TRUE, FALSE))
 
 # Predictive model: Logistic regression
 finals <- finals
