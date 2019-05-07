88479c54a2dda5e3fa21917d7c94173b449ca1cc
chrokh <christopher.okhravi@gmail.com>
2019-05-07 11:59:44 +0200

    Switch to RMarkdown.

---
+++ packages.R
@@ -0,0 +1,17 @@
+r = getOption('repos')
+r['CRAN'] = 'http://cran.us.r-project.org'
+options(repos = r)
+
+install.packages('tidyr')
+install.packages('ggplot2')
+install.packages('gridExtra')
+install.packages('remotes')
+remotes::install_github('rstudio/rmarkdown')
+
+
+install.packages('bindrcpp')
+# Solves:
+#   Error in library(p, character.only = TRUE) :
+#     there is no package called 'bindrcpp'
+#   Calls: render ... suppressPackageStartupMessages -> withCallingHandlers -> library
+# But I'm not sure why that's a problem.
