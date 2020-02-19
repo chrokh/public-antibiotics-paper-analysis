7926db2581d344bc213f808e87e5dd8afd58e3f2
chrokh <christopher.okhravi@gmail.com>
2020-02-19 16:54:40 +0100

    Predict the probability of a cost diff instead of the actual diff since indirect ENPV seems (very) non-linearly correlated to P(go).

--- packages.R
+++ packages.R
@@ -4,6 +4,8 @@ options(repos = r)
 
 install.packages('tidyr')
 install.packages('ggplot2')
+install.packages('broom')
+install.packages('forcats')
 install.packages('gridExtra')
 install.packages('triangle')
 install.packages('ggpubr')
@@ -11,9 +13,14 @@ install.packages('remotes')
 remotes::install_github('rstudio/rmarkdown')
 install.packages('bookdown')
 
-install.packages('bindrcpp')
+# For better nls (nlsLM)
+# See: https://rmazing.wordpress.com/2012/07/05/a-better-nls/
+# See: https://www.rdocumentation.org/packages/minpack.lm/versions/1.2-1/topics/nlsLM
+install.packages('minpack.lm')
+
 # Solves:
 #   Error in library(p, character.only = TRUE) :
 #     there is no package called 'bindrcpp'
 #   Calls: render ... suppressPackageStartupMessages -> withCallingHandlers -> library
 # But I'm not sure why that's a problem.
+install.packages('bindrcpp')
