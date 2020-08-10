6bce289a0b828847704b242d3168c5e255e5061c
chrokh <christopher.okhravi@gmail.com>
2020-08-10 07:12:24 +0200

    Add plots for thesis.

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+osascript -e 'display notification "Started" with title "Analysis"'
 rm _main.Rmd; time Rscript -e "bookdown::render_book('index.Rmd')"
+osascript -e 'display notification "Finished" with title "Analysis"'
